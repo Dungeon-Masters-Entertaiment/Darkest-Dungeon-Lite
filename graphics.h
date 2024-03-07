b3a314bfd50543a250c8a9b5de9d205c0cb94679
nov_ivan (Иван) <nov_ivan_777@mail.ru>
2024-03-07 16:27:49 +0300

    fixed cmake

--- graphics.h
+++ graphics.h
@@ -5,7 +5,16 @@
 #ifndef DARKEST_DUNGEON_LITE_GRAPHICS_H
 #define DARKEST_DUNGEON_LITE_GRAPHICS_H
 
+#include <ncurses.h>
+#include <vector>
 
+
+class Drawer{
+    int number;
+    Drawer(int num) {
+        number = num;
+    }
+};
 
 
 #endif //DARKEST_DUNGEON_LITE_GRAPHICS_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 //
 // Created by Егор Кулин on 29.02.2024.
 //
+#include <ncurses.h>
+#include <vector>
+
+//#include "graphics.h"
+
+
 
 int main(int argc, char **argv) {
+
+    initscr();
+
+    int row = 50, col = 50;
+
+    move(row / 2, col / 2);
+
+
+
+    refresh(); // обновить экран
+    getch(); // ждём нажатия символа
+
+    endwin(); // завершение работы с ncurses
 
 }
