a06dc52d3af38b39730ac1d920092b1dd730ce04
Rcmple <imaginelipa@ImagineLipa>
2024-03-07 19:26:47 +0300

    changed cmakelists

--- graphics.h
+++ graphics.h
@@ -1,11 +1,9 @@
 //
 // Created by Ivan Novikov on 07.03.2024.
 //
-
 #ifndef DARKEST_DUNGEON_LITE_GRAPHICS_H
 #define DARKEST_DUNGEON_LITE_GRAPHICS_H
 
-#include <ncurses.h>
 #include <vector>
 
 
--- main.cpp
+++ main.cpp
@@ -2,25 +2,23 @@
 // Created by Егор Кулин on 29.02.2024.
 //
 #include <ncurses.h>
-#include <vector>
-
-//#include "graphics.h"
-
-
-
 int main(int argc, char **argv) {
-
+    int x = 0;
+    int y = 0;
+    int c = 0;
     initscr();
-
-    int row = 50, col = 50;
-
-    move(row / 2, col / 2);
-
-
-
-    refresh(); // обновить экран
-    getch(); // ждём нажатия символа
-
+    keypad(stdscr, true);
+    noecho();
+    curs_set(0);
+    do{
+        clear();
+        if(c == KEY_UP) y--;
+        else if(c == KEY_DOWN) y++;
+        else if(c == KEY_LEFT) x--;
+        else if(c == KEY_RIGHT)x++;
+        mvaddch(y, x, '@');
+    }while((c = getch()) != 27);
+    getch();
     endwin(); // завершение работы с ncurses
 
 }
