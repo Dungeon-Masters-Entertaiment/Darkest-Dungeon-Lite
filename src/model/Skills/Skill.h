5f5275abe5d8413d052eef4766520283fa8a45dd
natalyasklyar <natasha@127.0.0.1>
2024-03-19 17:33:51 +0300

    Added class Skill.

---
+++ src/model/Skills/Skill.h
@@ -0,0 +1,39 @@
+//
+// Created by Natalya Sklyar on 19.03.2024.
+//
+
+#ifndef SKILL_H
+#define SKILL_H
+
+#include <string>
+#include <utility>
+
+enum class SkillType { MELEE, RANGED, MOVE, NONE, TELEPORT };
+
+class Skill {
+ protected:
+  std::string id;
+  SkillType type;
+  int atk;
+  std::pair<int, int> dmg;
+  int crit;
+  std::string effect;
+  int launch;
+  std::string targets;
+
+ public:
+  Skill();
+  Skill(std::string _id, SkillType _type, int _atk, std::pair<int, int> _dmg, int _crit, std::string _effect, int _launch, std::string _targets);
+
+  const std::string& getId() const;
+  SkillType getType() const;
+  int getAtk() const;
+  const std::pair<int, int>& getDmg() const;
+  int getCrit() const;
+  const std::string& getEffect() const;
+  int getLaunch() const;
+  const std::string& getTargets() const;
+};
+
+#endif // SKILL_H
+
