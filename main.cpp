d7791e2842daee99b6a8fa3f76d83fd93f5ba257
mregrock <78217843+mregrock@users.noreply.github.com>
2024-03-21 17:47:12 +0300

    Revert "Map saver"

--- main.cpp
+++ main.cpp
@@ -3,19 +3,25 @@
 //
 // #include "src/controller/Game.h"
 #include <iostream>
-// #include "view/graphics.h"
+//#include "view/graphics.h"
+
+
 
 #include "src/controller/Game.h"
-#include <controller/states/MapState.h>
-#include <fstream>
-#include <jsonParser/JSONParser.h>
-#include "model/MapSaver/MapSaver.h"
+
+
 
 int main(int argc, char **argv) {
-  AntohaFabric generator;
-  // // Use the map instance to generate the map
-  static Map map = generator.Build(50, 50);
+    /*AntohaFabric generator;
+    // Use the map instance to generate the map
+    static Map map = generator.Build(50, 50);
+    map.Draw();
+    */
+    
+    
+    //cur.divide_screen();
+    Game game;
+    game.Start();
+    return 0;
 
-
-  MapSaver::save(&map);
 }
