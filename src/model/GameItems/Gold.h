f233df4e5af7a20545dde3dde2c7e76a5eb8ca3b
TheEffectik <andruxa-mix112@yandex.ru>
2024-03-21 16:36:47 +0300

    Add Gold Item

---
+++ src/model/GameItems/Gold.h
@@ -0,0 +1,24 @@
+#ifndef PEALINGPOTION_H
+#define PEALINGPOTION_H
+#include "Items.h"
+#include <iostream>
+
+class Gold : public Item{
+public:
+    Gold() {
+        count = 0;
+    }
+    ~Gold() override = default;   
+    void use() override {};
+    int count;
+    std::string getDescription() const override{
+        return "This healing potion adds 4 - 6 health to the selected character";
+    }
+    void add(int amount) {
+        count += amount;
+    }
+    void sub(int amount) {
+        count -= amount;
+    }
+};
+#endif
